99522426b269a76446868e8960ec82cd5ae4ede8
alvaroximenesrj <alvaroximenesrj@gmail.com>
2019-04-15 00:54:25 -0300

    scaffolding + EF done

---
+++ MusicStore/Models/MusicStoreDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreDB : DbContext
+    {
+       
+    
+        public MusicStoreDB() : base("name=MusicStoreDB")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MusicStore.Models.Album> Albums { get; set; }
+
+        public System.Data.Entity.DbSet<MusicStore.Models.Artist> Artists { get; set; }
+
+        public System.Data.Entity.DbSet<MusicStore.Models.Genre> Genres { get; set; }
+    }
+}
